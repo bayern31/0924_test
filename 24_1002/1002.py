7d0bb4bd008ff39d4564362aa21ccfa2d30e1509
dndtn@DESKTOP-NIAI25F <dndtn6763@gamail.com>
2024-10-02 11:31:49 +0900

    uadated

--- 24_1002/1002.py
+++ 24_1002/1002.py
@@ -9,4 +9,21 @@ data = [
 columns = ["종목코드", "종목명", "현재가"]
 df = DataFrame(data=data, columns=columns)
 df.set_index("종목코드", inplace=True)
-print(df)
+
+# ascending=False : 내림차순
+# ascending=True (기본값) : 오름차순
+#print(df.sort_values(by="종목명", ascending=False)) 
+#print(df.sort_values(by="종목명", ascending=True))
+
+#print(df.sort_index()) # 기본값
+#print(df.sort_index(ascending=False)) # 역순
+
+# 인덱스 연산
+# 합집합, 교집합, 차집합의 원리를 이용해서, 데이터 병합을 할 때 사용
+import pandas as pd
+idx1 = pd.Index([1, 2, 3])
+idx2 = pd.Index([2, 3, 4])
+
+print(idx1.union(idx2))
+print(idx1.intersection(idx2))
+print(idx1.difference(idx2))
